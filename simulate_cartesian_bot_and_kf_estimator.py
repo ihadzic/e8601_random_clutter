02edcab420c09523815368968527702ab42d52cf
Ilija Hadzic <ilija.hadzic@nokia-bell-labs.com>
2024-05-16 00:12:28 -0400

    calculate and plot the position estimate.
    Signed-off-by: Ilija Hadzic <ilija.hadzic@nokia-bell-labs.com>

--- simulate_cartesian_bot_and_kf_estimator.py
+++ simulate_cartesian_bot_and_kf_estimator.py
@@ -3,6 +3,7 @@ from cartesian_bot_kf import CartesianBotKF
 import matplotlib.pyplot as plt
 import numpy
 import math
+import random
 
 # motor parameters
 K = 0.01
@@ -36,8 +37,27 @@ vx = [v+n for v, n in zip(vx_nominal, list(numpy.random.normal(0, math.sqrt(vx_v
 vy_nominal = [5 if t < 7 and t > 2 else 0 for t in all_t]
 vy = [v+n for v, n in zip(vy_nominal, list(numpy.random.normal(0, math.sqrt(vx_variance), len(all_t))))]
 
+# uncertainty range and correlation for measurements
+min_var_z = 0.01
+max_var_z = 0.2
+min_rho_z = 0
+max_rho_z = 0.7
+
 x_ground_truth = []
 y_ground_truth = []
+x_estimate = []
+y_estimate = []
+
+def generate_measurement(xm, ym):
+    var_x = random.uniform(min_var_z, max_var_z)
+    var_y = random.uniform(min_var_z, max_var_z)
+    rho = random.uniform(min_rho_z, max_rho_z)
+    var_xy = rho * math.sqrt(var_x) * math.sqrt(var_y)
+    cov = numpy.array([[var_x, var_xy],
+                       [var_xy, var_y]])
+    xz, yz = numpy.random.multivariate_normal([xm, ym], cov)
+    return xz, yz, var_x, var_y, rho
+
 for i in range(len(all_t)):
     # move the system under simulation using noisy input
     bot_model._predict(all_t[i], vx[i], vy[i])
@@ -45,6 +65,14 @@ for i in range(len(all_t)):
     xgt, ygt = bot_model._peek_pos()
     x_ground_truth.append(xgt)
     y_ground_truth.append(ygt)
+    zx, zy, var_zx, var_zy, rho_zxy = generate_measurement(xgt, ygt)
+    estimator.advance_filter(all_t[i], vx[i], vy[i], zx, zy,
+                             vx_variance, vy_variance,
+                             var_zx, var_zy, rho_zxy)
+    est_xy, est_xy_cov = estimator.get_estimate()
+    est_x, est_y = est_xy[0, 0], est_xy[1, 0]
+    x_estimate.append(est_x)
+    y_estimate.append(est_y)
 
 plt.figure(1)
 
@@ -81,6 +109,14 @@ plt.figure(3)
 plt.scatter(x_ground_truth, y_ground_truth, s=4)
 plt.xlabel('x [m]')
 plt.ylabel('y [m]')
+plt.title('position -- ground truth')
+plt.grid()
+
+plt.figure(4)
+plt.scatter(x_estimate, y_estimate, s=4)
+plt.xlabel('x [m]')
+plt.ylabel('y [m]')
+plt.title('position -- estimate')
 plt.grid()
 
 plt.show()
