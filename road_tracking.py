229f0282f55d061e34d50c5133f3b1d305ab8e11
Ilija Hadzic <ih2435@columbia.edu>
2024-07-06 23:14:17 -0400

    road_track: rework to use classes
    Signed-off-by: Ilija Hadzic <ih2435@columbia.edu>

--- road_tracking.py
+++ road_tracking.py
@@ -3,43 +3,50 @@ import matplotlib.pyplot as plt
 import random
 import time
 
-def plot_road(ax, x_vert = 10, y_horiz = 10, road_width = 1):
-    x1, x2 = x_vert - road_width / 2, x_vert + road_width / 2
-    y1, y2 = y_horiz - road_width / 2, y_horiz + road_width / 2
 
-    # road edges
-    ax.plot([x1, x1], [0, y1], color='black')
-    ax.plot([x2, x2], [0, y2], color='black')
-    ax.plot([x1, -road_width], [y1, y1], color='black')
-    ax.plot([x2, -road_width], [y2, y2], color='black')
+class RoadTrackSim:
+    def __init__(self, x_vert, y_horiz, road_width):
+        self.fig, self.ax = plt.subplots()
+        self.gt_plot, = self.ax.plot([], [], 'bx')
+        self.x_vert = x_vert
+        self.y_horiz = y_horiz
+        self.road_width = road_width
+        self.x1 = self.x_vert - self.road_width / 2
+        self.x2 = self.x_vert - self.road_width / 2
+        self.y1 = self.y_horiz - self.road_width / 2
+        self.y2 = self.y_horiz + self.road_width / 2
+        self._plot_road()
+        self.fig.gca().set_aspect('equal', adjustable='box')
 
-    # fill the insides of the road
-    road_vertices = [
-        (x1, 0), (x1, y1), (-road_width, y1),
-        (-road_width, y2), (x2, y2), (x2, 0)
-    ]
-    ax.fill(*zip(*road_vertices), color='gray', alpha=0.5)
+    def _plot_road(self):
+        # road edges
+        self.ax.plot([self.x1, self.x1], [0, self.y1], color='black')
+        self.ax.plot([self.x2, self.x2], [0, self.y2], color='black')
+        self.ax.plot([self.x1, -self.road_width], [self.y1, self.y1], color='black')
+        self.ax.plot([self.x2, -self.road_width], [self.y2, self.y2], color='black')
 
-    # set the plot limits
-    ax.set_xlim(-road_width, x_vert + road_width)
-    ax.set_ylim(0, y_horiz + road_width)
-    ax.grid()
+        # fill the insides of the road
+        road_vertices = [
+            (self.x1, 0), (self.x1, self.y1), (-self.road_width, self.y1),
+            (-self.road_width, self.y2), (self.x2, self.y2), (self.x2, 0)
+        ]
+        self.ax.fill(*zip(*road_vertices), color='gray', alpha=0.5)
 
-def redraw(fig, gt_plot):
-    gt_plot.set_data(random.uniform(0, 4), random.uniform(0, 4))
-    fig.canvas.draw()
-    fig.canvas.flush_events()
+        # set the plot limits
+        self.ax.set_xlim(-self.road_width, self.x_vert + self.road_width)
+        self.ax.set_ylim(0, self.y_horiz + self.road_width)
+        self.ax.grid()
 
-fig, ax = plt.subplots()
-plot_road(ax, x_vert=5, y_horiz=10, road_width=2)
-gt_plot, = ax.plot([], [], 'bx')
+    def redraw(self):
+        self.gt_plot.set_data(random.uniform(0, 4), random.uniform(0, 4))
+        self.fig.canvas.draw()
+        self.fig.canvas.flush_events()
 
+road_track = RoadTrackSim(x_vert = 5, y_horiz = 10, road_width = 2)
 # set aspect ratio to equal for correct representation
-fig.gca().set_aspect('equal', adjustable='box')
 plt.ion()
 plt.show()
 
 for i in range(50):
-    redraw(fig, gt_plot)
+    road_track.redraw()
     time.sleep(0.5)
-
