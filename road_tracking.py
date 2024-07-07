7b3f57691a212a0da41076a7c444e53a28d640ce
Ilija Hadzic <ih2435@columbia.edu>
2024-07-06 22:17:05 -0400

    road_tracking: rework the plot in preparation for using animations
    Signed-off-by: Ilija Hadzic <ih2435@columbia.edu>

--- road_tracking.py
+++ road_tracking.py
@@ -1,30 +1,33 @@
 #!/usr/bin/env python
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
-def plot_road(x_vert, y_horiz, road_width):
+def plot_road(ax, x_vert = 10, y_horiz = 10, road_width = 1):
     x1, x2 = x_vert - road_width / 2, x_vert + road_width / 2
     y1, y2 = y_horiz - road_width / 2, y_horiz + road_width / 2
 
     # road edges
-    plt.plot([x1, x1], [0, y1], color='black')
-    plt.plot([x2, x2], [0, y2], color='black')
-    plt.plot([x1, -road_width], [y1, y1], color='black')
-    plt.plot([x2, -road_width], [y2, y2], color='black')
+    ax.plot([x1, x1], [0, y1], color='black')
+    ax.plot([x2, x2], [0, y2], color='black')
+    ax.plot([x1, -road_width], [y1, y1], color='black')
+    ax.plot([x2, -road_width], [y2, y2], color='black')
 
     # fill the insides of the road
     road_vertices = [
         (x1, 0), (x1, y1), (-road_width, y1),
         (-road_width, y2), (x2, y2), (x2, 0)
     ]
-    plt.fill(*zip(*road_vertices), color='gray', alpha=0.5)
+    ax.fill(*zip(*road_vertices), color='gray', alpha=0.5)
 
     # set the plot limits
-    plt.xlim(-road_width, x_vert + road_width)
-    plt.ylim(0, y_horiz + road_width)
-    plt.grid()
+    ax.set_xlim(-road_width, x_vert + road_width)
+    ax.set_ylim(0, y_horiz + road_width)
+    ax.grid()
 
-    # set aspect ratio to equal for correct representation
-    plt.gca().set_aspect('equal', adjustable='box')
-    plt.show()
+fig, ax = plt.subplots()
+plot_road(ax, x_vert=5, y_horiz=10, road_width=2)
 
-plot_road(x_vert=5, y_horiz=10, road_width=2)
+# set aspect ratio to equal for correct representation
+fig.gca().set_aspect('equal', adjustable='box')
+plt.show()
+
